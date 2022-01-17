a3b339996a44fc9c6e9c1e8dedafb77a15eca225
HyunTaeChoi <chsachd1231@gmail.com>
2022-01-17 14:32:51 +0900

    1234

--- src/main/java/com/uplus/msa/entity/Customer.java
+++ src/main/java/com/uplus/msa/entity/Customer.java
@@ -5,7 +5,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
 public class Customer {
 	
 	@Id @GeneratedValue
